cf02eebcadbd3722b4b00d2c37853a0d962085f2
moskv11n <162998410+moskv11n@users.noreply.github.com>
2024-05-22 12:41:15 +0300

    Add files via upload

---
+++ build/CMakeFiles/Design.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Design.dir/main-2.cpp.o"
+  "CMakeFiles/Design.dir/main-2.cpp.o.d"
+  "Design"
+  "Design.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
